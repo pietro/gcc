iains/gcc-14-branch

c-family: Support cross-language keywords as an extension.
This allows us compatibility with clang extensions permitting
C keywords in C++ and vice versa (initially implementing only
two to deal with specific SDK issues).

Addresses issue #142.

gcc/c-family/ChangeLog:

	* c-common.cc (flag_allow_extra_keywords): Mark
	_Alignas and _Alignof as usable in C++ when allowed.
	* c-common.h (D_EXT_C_IN_CXX, D_EXT_CXX_IN_C): New.
	* c.opt (flag_allow_extra_keywords): New.

gcc/ChangeLog:

	* config/darwin.cc (darwin_override_options): If
	flag_allow_extra_keywords is not explicitly set then
	switch it on for Darwin.

gcc/cp/ChangeLog:

	* lex.cc (init_reswords): Handle D_EXT_C_IN_CXX.

Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

gcc/c-family/c-common.h
@@ -445,6 +445,8 @@ extern machine_mode c_default_pointer_mode;
 #define D_CXX20		0x8000  /* In C++, C++20 only.  */
 #define D_CXX_COROUTINES 0x10000  /* In C++, only with coroutines.  */
 #define D_CXX_MODULES	0x20000  /* In C++, only with modules.  */
+#define D_EXT_C_IN_CXX	0x40000  /* In C++, allow additional C keywords.  */
+#define D_EXT_CXX_IN_C	0x80000  /* In C, allow additional C++ keywords.  */
 
 #define D_CXX_CONCEPTS_FLAGS D_CXXONLY | D_CXX_CONCEPTS
 #define D_CXX_CHAR8_T_FLAGS D_CXXONLY | D_CXX_CHAR8_T

pietro/gcc

gcc/c-family/c-common.h
@@ -455,6 +455,8 @@ extern machine_mode c_default_pointer_mode;
 #define D_CXX_COROUTINES 0x10000  /* In C++, only with coroutines.  */
 #define D_CXX_MODULES	0x20000  /* In C++, only with modules.  */
 #define D_CXX26         0x40000	/* In C++, C++26 only.  */
+#define D_EXT_C_IN_CXX	0x80000  /* In C++, allow additional C keywords.  */
+#define D_EXT_CXX_IN_C	0x100000  /* In C, allow additional C++ keywords.  */
 
 #define D_CXX_CONCEPTS_FLAGS D_CXXONLY | D_CXX_CONCEPTS
 #define D_CXX_CHAR8_T_FLAGS D_CXXONLY | D_CXX_CHAR8_T